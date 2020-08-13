cc4eca63f397c7e5d116a86e1f15b73b7ac5b353
RAJA MISHRA <rajamishra0702@gmail.com>
2020-08-13 16:17:13 +0530

    Update settings.py

--- ransap/settings.py
+++ ransap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "w&kc&oj(g-=hm=3ytq_6vu)dzynts^%$bu3z0(#l790@j8t&t5"
+SECRET_KEY = "######################"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
